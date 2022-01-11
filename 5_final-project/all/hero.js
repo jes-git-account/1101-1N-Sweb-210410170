2ac99ddc2df10ccf471ae85f4c54d7fda8979f56
31461057 <lling.31461054@gmail.com>
2022-01-11 12:29:46 +0800

    k

--- 5_final-project/all/hero.js
+++ 5_final-project/all/hero.js
@@ -26,3 +26,13 @@ function showSlides(n) {
 
   dots[slideIndex-1].className += " active";
 }
+/*                        */
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
+});
+
+////////////////////////////////////////////////////////
+// Sticky navigation
