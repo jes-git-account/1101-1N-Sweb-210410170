e9c77ee4b411b2cf8337e9a0f55d2c7f3f111571
31461057 <lling.31461054@gmail.com>
2022-01-17 17:37:23 +0800

    i

--- 5_final-project/all/meun.js
+++ 5_final-project/all/meun.js
@@ -1,3 +1,13 @@
+
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
+});
+
+////////////////////////////////////////////////////////
+// Sticky navigation
 const sectionHeroEl = document.querySelector('.section-hero');
 const obs = new IntersectionObserver(
   function (entries) {
